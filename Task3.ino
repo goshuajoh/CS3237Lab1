26ada0097d2b1307bd3900cf4072fbb38fead84c
Joshua Goh <74043789+goshuajoh@users.noreply.github.com>
2024-08-30 22:01:18 +0800

    Update Task3.ino

--- Task3.ino
+++ Task3.ino
@@ -1,5 +1,5 @@
 #define SWITCH 4
-byte state = LOW;
+int state = 0;
 unsigned long lastDebounceTime = 0;  // Last time the switch was toggled
 unsigned long debounceDelay = 50;    // Debounce time in milliseconds
 
@@ -13,7 +13,7 @@ void loop() {
 
   // Check if the switch has been pressed (reading LOW) and debounce the input
   if (reading == LOW && (millis() - lastDebounceTime) > debounceDelay) {
-    state = !state;  // Toggle the state
+    state = !state;  // Toggle between states
     lastDebounceTime = millis();  // Update the last debounce time
   }
 
@@ -23,5 +23,5 @@ void loop() {
     Serial.println("Toggle Off");
   }
 
-  delay(10);  // Small delay to make the serial output readable
+  delay(10);
 }
